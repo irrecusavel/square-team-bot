c8fcd48efbf6a491b78a549f9e9e10423b0ab8ff
josejooj - hvm <ribamarjunior@protonmail.com>
2023-10-09 11:53:57 -0300

    Create and configure axios for Square Cloud API, add ENV verifications

--- CustomClient.ts
+++ CustomClient.ts
@@ -1,22 +1,37 @@
 import { Client, ClientOptions } from 'discord.js';
 import fs from 'fs';
 import { Event } from './interfaces/discord';
+import axios from 'axios';
 
 class CustomClient extends Client {
+
+    moderators = [];
+
     constructor(options: ClientOptions & { token: string }) {
         super(options)
         this.EventHandler();
-        this.login(options.token)
+        this.once('ready', this.Configure);
+        this.login(options.token).catch(e => {
+            console.error("Your env.DISCORD_TOKEN is invalid. Please re-configure.")
+            process.exit()
+        })
+    }
+
+    private async Configure() {
+
+        const res = await axios.get('/user');
+
+        this.application!.owner = await this.users.fetch(res.data.response.user.id)
+
     }
 
     private EventHandler() {
         for (const filename of fs.readdirSync('./dist/events')) {
-            
             const data = require("./events/" + filename).default as Event;
-            
             this.on(data.name, data.listener)
         }
     }
+
 }
 
 export {
--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 import { CustomClient } from './CustomClient'
 import { config as configEnv } from 'dotenv';
+import axios from 'axios';
 import './util/Prototypes';
 
 configEnv();
+
+axios.defaults.baseURL = "https://api.squarecloud.app/v2"
+axios.defaults.headers.common.Authorization = process.env.SQUARE_CLOUD_API_KEY
+axios.interceptors.response.use((response) => response, (error) => {
+    
+    if (error.response?.status === 401) {
+        console.error("Your env.SQUARE_CLOUD_API_KEY is invalid. Please re-configure.");
+        process.exit()
+    }
+    
+    error.response
+})
 
 new CustomClient({
     intents: [],
